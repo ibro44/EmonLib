c9ed6405ddabc6cdd551e7e781bca6e1b349ddc3
ibro44 <ibro@aol.com>
2014-12-09 13:06:00 -0600

    Update EmonLib.cpp

--- firmware/EmonLib.cpp
+++ firmware/EmonLib.cpp
@@ -7,9 +7,9 @@
 */
 
 //#include "WProgram.h" un-comment for use on older versions of Arduino IDE
-#include "EmonLib.h"
-
+#include "Emonlib.h"
 #include "application.h"
+#include "math.h"
 
 //--------------------------------------------------------------------------------------
 // Sets the pins to be used for voltage and current sensors
@@ -28,14 +28,36 @@ void EnergyMonitor::current(int _inPinI, double _ICAL)
 }
 
 //--------------------------------------------------------------------------------------
+// Sets the pins to be used for voltage and current sensors based on emontx pin map
+//--------------------------------------------------------------------------------------
+void EnergyMonitor::voltageTX(double _VCAL, double _PHASECAL)
+{
+   inPinV = 2;
+   VCAL = _VCAL;
+   PHASECAL = _PHASECAL;
+}
+
+void EnergyMonitor::currentTX(int _channel, double _ICAL)
+{
+   if (_channel == 1) inPinI = 3;
+   if (_channel == 2) inPinI = 0;
+   if (_channel == 3) inPinI = 1;
+   ICAL = _ICAL;
+}
+
+//--------------------------------------------------------------------------------------
 // emon_calc procedure
 // Calculates realPower,apparentPower,powerFactor,Vrms,Irms,kwh increment
 // From a sample window of the mains AC voltage and current.
 // The Sample window length is defined by the number of half wavelengths or crossings we choose to measure.
 //--------------------------------------------------------------------------------------
-void EnergyMonitor::calcVI(int crossings, unsigned int timeout)
+void EnergyMonitor::calcVI(int crossings, int timeout)
 {
+   #if defined emonTxV3
 	int SUPPLYVOLTAGE=3300;
+   #else 
+	int SUPPLYVOLTAGE = readVcc();
+   #endif
 
   int crossCount = 0;                             //Used to measure number of times threshold is crossed.
   int numberOfSamples = 0;                        //This is now incremented  
@@ -43,7 +65,7 @@ void EnergyMonitor::calcVI(int crossings, unsigned int timeout)
   //-------------------------------------------------------------------------------------------------------------------------
   // 1) Waits for the waveform to be close to 'zero' (500 adc) part in sin curve.
   //-------------------------------------------------------------------------------------------------------------------------
-  boolean st=false;                                  //an indicator to exit the while loop
+  bool st=false;                                  //an indicator to exit the while loop
 
   unsigned long start = millis();    //millis()-start makes sure it doesnt get stuck in the loop if there is an error.
 
@@ -144,8 +166,14 @@ void EnergyMonitor::calcVI(int crossings, unsigned int timeout)
 //--------------------------------------------------------------------------------------
 double EnergyMonitor::calcIrms(int NUMBER_OF_SAMPLES)
 {
+  
+   #if defined emonTxV3
 	int SUPPLYVOLTAGE=3300;
+   #else 
+	int SUPPLYVOLTAGE = readVcc();
+   #endif
 
+  
   for (int n = 0; n < NUMBER_OF_SAMPLES; n++)
   {
     lastSampleI = sampleI;
@@ -185,3 +213,41 @@ void EnergyMonitor::serialprint()
     delay(100); 
 }
 
+//thanks to http://hacking.majenko.co.uk/making-accurate-adc-readings-on-arduino
+//and Jérôme who alerted us to http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+
+long EnergyMonitor::readVcc() {
+  long result;
+  
+  //not used on emonTx V3 - as Vcc is always 3.3V - eliminates bandgap error and need for calibration http://harizanov.com/2013/09/thoughts-on-avr-adc-accuracy/
+
+  #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328__) || defined (__AVR_ATmega328P__)
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);  
+  #elif defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_AT90USB1286__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  ADCSRB &= ~_BV(MUX5);   // Without this the function always returns -1 on the ATmega2560 http://openenergymonitor.org/emon/node/2253#comment-11432
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+	
+  #endif
+
+
+  #if defined(__AVR__) 
+  delay(2);                                        // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC);                             // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1126400L / result;                     //1100mV*1024 ADC steps http://openenergymonitor.org/emon/node/1186
+  return result;
+ #elif defined(__arm__)
+  return (3300);                                  //Arduino Due
+ #else 
+  return (3300);                                  //Guess that other un-supported architectures will be running a 3.3V!
+ #endif
+}
+
+  
+ 
